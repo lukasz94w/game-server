0a5099264b4369287bd77fa7030b7dedfa2ade37
lukasz94w <lukasz94w@wp.pl>
2023-09-28 20:02:19 +0200

    Having fun with coding TCP server part 1

--- src/main/java/pl/lukasz94w/Main.java
+++ src/main/java/pl/lukasz94w/Main.java
@@ -1,8 +1,10 @@
 package pl.lukasz94w;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TCPServer server = new TCPServer(9999);
     }
 
     public static int someTestMethodForJenkinsTesting(int number1, int number2) {
