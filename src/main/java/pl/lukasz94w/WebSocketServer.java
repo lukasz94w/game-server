a1ecf103f06202c861eab15ff79ad71e113dd377
lukasz94w <lukasz94w@wp.pl>
2023-10-29 19:49:28 +0100

    Add session pairings

--- src/main/java/pl/lukasz94w/WebSocketServer.java
+++ src/main/java/pl/lukasz94w/WebSocketServer.java
@@ -1,5 +1,8 @@
 package pl.lukasz94w;
 
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -8,20 +11,24 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 import java.time.LocalTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public class WebSocketServer extends TextWebSocketHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
     private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    private int numberOfOpenedSessions = 0;
+    private final LinkedHashMap<WebSocketSession, WebSocketSession> pairedSessions = new LinkedHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("Server connection opened, session id: " + session.getId());
-        //logger.info("Server connection opened");
-        sessions.add(session);
-        TextMessage message = new TextMessage("one-time message from server");
-        //logger.info("Server sends: {}", message);
-        session.sendMessage(message);
+        logger.info("Server connection opened with session id: {}", session.getId());
+        handleSessionsNumbers(session);
+        handleSessionsPairing(session);
     }
 
     @Override
@@ -43,13 +50,16 @@ public class WebSocketServer extends TextWebSocketHandler {
     }
 
     @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String request = message.getPayload();
-        System.out.println("Server received: " + request + " from user with session id: " + session.getId());
-        //logger.info("Server received: {}", request);
-//        String response = String.format("response from server to '%s'", HtmlUtils.htmlEscape(request));
-        //logger.info("Server sends: {}", response);
-        session.sendMessage(new TextMessage("Hello user: " + session.getId() + ", that's also your original message: " + request));
+    public void handleTextMessage(WebSocketSession messagingSession, TextMessage message) throws Exception {
+        WebSocketSession pairedSession = findPairedSession(messagingSession);
+
+        if (pairedSession != null) {
+            JSONObject o = new JSONObject(message.getPayload());
+            String extractedMessage = o.getString("message");
+            pairedSession.sendMessage(new TextMessage(extractedMessage));
+        } else {
+            messagingSession.sendMessage(new TextMessage("Please wait for the next player to join..."));
+        }
     }
 
     @Override
@@ -62,4 +72,42 @@ public class WebSocketServer extends TextWebSocketHandler {
 //    public List<String> getSubProtocols() {
 //        return Collections.singletonList("subprotocol.demo.websocket");
 //    }
+
+    private void handleSessionsNumbers(WebSocketSession session) {
+        sessions.add(session);
+        numberOfOpenedSessions++;
+    }
+
+    private void handleSessionsPairing(WebSocketSession newSession) throws IOException {
+        if (numberOfOpenedSessions % 2 != 0) {
+            pairedSessions.put(newSession, null);
+            newSession.sendMessage(new TextMessage("Successfully connected. Waiting for another player to join..."));
+        } else {
+            WebSocketSession lastSessionWithoutAssignedPair = pairedSessions.lastEntry().getKey();
+            pairedSessions.put(lastSessionWithoutAssignedPair, newSession);
+            lastSessionWithoutAssignedPair.sendMessage(new TextMessage("Your opponent has connected. Let's the party started!"));
+            newSession.sendMessage(new TextMessage("Successfully connected. Good luck!"));
+        }
+
+        logger.info("Summarize of current sessions: {}", pairedSessions);
+    }
+
+    private WebSocketSession findPairedSession(WebSocketSession messagingSession) {
+        WebSocketSession pairedSession;
+
+        // find by key
+        pairedSession = pairedSessions.get(messagingSession);
+        if (pairedSession != null) {
+            return pairedSession;
+        }
+
+        // find by value
+        Set<Map.Entry<WebSocketSession, WebSocketSession>> sessionEntries = pairedSessions.entrySet();
+        for (Map.Entry<WebSocketSession, WebSocketSession> sessionEntry : sessionEntries) {
+            if (sessionEntry.getValue().equals(messagingSession))
+                pairedSession = sessionEntry.getKey();
+        }
+
+        return pairedSession;
+    }
 }
