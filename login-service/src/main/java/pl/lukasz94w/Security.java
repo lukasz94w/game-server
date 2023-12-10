810d8c029067e7a106b61ca54427f366c5da172d
lukasz94w <lukasz94w@wp.pl>
2023-12-10 22:10:02 +0100

    Include progress of work on login service from 10th of December

---
+++ login-service/src/main/java/pl/lukasz94w/Security.java
@@ -0,0 +1,48 @@
+package pl.lukasz94w;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+
+@Configuration
+public class Security {
+
+    @Bean
+    public PasswordEncoder getBcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager get() {
+        UserDetails user = User.withUsername("jan")
+                .password(getBcryptPasswordEncoder().encode("jan123"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(getBcryptPasswordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(Arrays.asList(user, admin));
+    }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/for-user").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/for-admin").hasAuthority("ROLE_ADMIN")
+//                )
+//                .formLogin(
+//                        .formLogin.permitAll())
+//                .logout()
+//                .logoutSuccessUrl("/bye").permitAll();
+//        return http.build();
+//    }
+}
